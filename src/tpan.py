b5c41093fcced3c3b7287ad516f4de2aae2af13d
Kishan Kumar <kishan.kumar@bankbazaar.com>
2018-09-19 12:25:41 +0530

    pan OCR script modifications to get exact details

--- src/tpan.py
+++ src/tpan.py
@@ -33,6 +33,7 @@ for y in range(img.size[1]):
 text = pytesseract.image_to_string(img)
 text = filter(lambda x: ord(x)<128,text)
 
+print "extracted complete text"
 print text
 
 # Initializing data variable
@@ -57,7 +58,8 @@ for lin in lines:
 	text1.append(s)
 
 text1 = filter(None, text1)	
-print(text1)
+#print "text1"
+#print(text1)
 lineno=0
 
 for wordline in text1:
@@ -67,7 +69,8 @@ for wordline in text1:
 		break
 
 text0 = text1[lineno+1:]
-print(text0)
+#print "text0"
+#print(text0)
 
 #-----------Read Database
 with open('namedb.csv', 'rb') as f:
@@ -78,39 +81,59 @@ newlist = sum(newlist, [])
 # Searching for Name and finding closest name in database
 try:
 	for x in text0:
+		#print("x")
+		#print(x)
 		for y in x.split( ):
+		#	print("y")
+		#	print(y)
+			#y = filter(lambda xyz: xyz.isalpha(), y)
 			if(difflib.get_close_matches(y.upper(), newlist)):
+				#nameline.append(x)
+#				xx = filter(lambda xyz: xyz.isalpha(), x)
 				nameline.append(x)
+
 				break
 except:
 	pass
 
 try:
+	#print("nameline")
+	#print(nameline)
+	#print("nameline end")
 	name = nameline[0]
-	fname = nameline[1]
+	#fname = nameline[1] # because nameline[1] contains Govt. Of India.
+	fname = re.sub('^[^a-zA-z]*|[^a-zA-Z]*$','',nameline[2]) #because some numbers are getting appended at the end of father's name
 except:
 	pass
 	
 try:
 	dobline = [item for item in text0 if item not in nameline]
-	print dobline
+	#print "dobline"
+	#print dobline
 	for x in dobline:
 		z = x.split()
 		z = [s for s in z if len(s) > 3]
 		for y in z:
 			if(dparser.parse(y, fuzzy=True)):
-				dob = dparser.parse(y,fuzzy=True).year
+				dobParsed = dparser.parse(y,fuzzy=True,dayfirst=True)
+				dob = dobParsed.strftime("%B %d, %Y")
+				#dob = dparser.parse(y,fuzzy=True,dayfirst=True).year
 				panline = dobline[dobline.index(x)+1:]
 				break
 except:
 	pass
 	
 try:
+	#print("panline")
+	#print(panline)
 	for wordline in panline:
 		xx = wordline.split( )
-		if ([w for w in xx if re.search('(Number|umber|Account|ccount|count|Permanent|ermanent|manent)$', w)]):
-			pan = panline[panline.index(wordline)+1]
-			break
+		#print("xx")
+		#print(xx)
+		#if ([w for w in xx if re.search('(Number|umber|Account|ccount|count|Permanent|ermanent|manent)$', w)]):
+			#pan = panline[panline.index(wordline)+1]
+		pan = panline[panline.index(wordline)]
+			#break
 	pan = pan.replace(" ", "")
 except:
 	pass
@@ -122,6 +145,7 @@ data['Father Name'] = fname
 data['Date of Birth'] = dob
 data['PAN'] = pan
 
+print "final details"
 print name
 print fname
 print dob
